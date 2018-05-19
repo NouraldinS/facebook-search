0be0660b05b49ebc291d50e38daaf8ae3ab65bd0
= <noureldean.saed@gmail.com>
2018-05-19 15:13:16 +0300

    m

--- src/controllers/postPage.js
+++ src/controllers/postPage.js
@@ -2,5 +2,6 @@ const axios = require('axios');
 
 module.exports = (req, res) => {
   axios.get('https://graph.facebook.com/v3.0/MaanNews.net/posts?fields=created_time,description,full_picture,message,message_tags,name,picture,attachments{description,media,title,type},likes,with_tags,id,link&since=1483228800&until=1596275200&limit=100')
-    .then(fbRes => console.log(fbRes));
+    .then(fbRes => console.log(fbRes))
+    .catch(err => console.log(err));
 };
