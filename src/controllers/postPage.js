3e01668b4e013f6a9e032f979b7a19bc501e756a
= <noureldean.saed@gmail.com>
2018-05-19 16:06:00 +0300

    m

--- src/controllers/postPage.js
+++ src/controllers/postPage.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports = (req, res) => {
-  req.body.pageid = 111029928948046; // MA'AN El Ekhbariah
+  req.body = { pageid: 111029928948046 }; // MA'AN El Ekhbariah
   const { pageid } = req.body;
   axios.get(`https://graph.facebook.com/v3.0/${pageid}/feed?fields=created_time,description,full_picture,message,message_tags,name,picture,attachments{description,media,title,type},likes,with_tags,id,link&since=1483228800&until=1596275200&limit=100`)
     .then(fbRes => console.log(fbRes))
